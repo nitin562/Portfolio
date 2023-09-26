503eebdbf4c004f6f840c9de134248afa084456d
nitin562 <nitindbas8800@gmail.com>
2023-09-26 19:07:36 +0530

    Adding NavBar

---
+++ frontend/src/Component/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavBar from '../NavBar/NavBar'
+
+export default function Home() {
+  return (
+    <div className='Home' style={{width:"100vw",height:"100vh"}}>
+        <NavBar/>
+    </div>
+  )
+}
