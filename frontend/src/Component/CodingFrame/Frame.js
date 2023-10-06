29b75224bb31f7f385264cf439b3ece1d3c114a3
nitin562 <nitindbas8800@gmail.com>
2023-10-06 11:03:57 +0530

    Frame.js Created and completed

---
+++ frontend/src/Component/CodingFrame/Frame.js
@@ -0,0 +1,29 @@
+import React,{useRef,useEffect} from 'react'
+import "./frame.css"
+import Typed from "typed.js"
+export default function Frame() {
+    const typeArea=useRef(0)
+    useEffect(() => {
+        const typed = new Typed(typeArea.current, {
+          strings: ["Frontend Developer","Backend Developer","Full Stack Developer","MERN Developer"],
+          typeSpeed: 50,
+          loop:true,
+          
+        });
+    
+        return () => {
+          // Destroy Typed instance during cleanup to stop animation
+          typed.destroy();
+        };
+      }, []);
+  return (
+    <div id="frame">
+        <p className="CodeSign">
+            <span>&lt;</span><span>/</span><span>&gt;</span>
+        </p>
+        <div className="TypeProfile">
+          <span ref={typeArea}></span>
+        </div>
+    </div>
+  )
+}
--- frontend/src/Component/Home.js
+++ frontend/src/Component/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
-
 import './home.css'
 import Nav from './Nav/Nav'
+import Frame from './CodingFrame/Frame'
 export default function Home() {
   return (
     <div id='Home'>
-    
         <Nav/>
+        <div className="IntroLine">
+          <Frame/>
+        </div>
+        
     </div>
   )
 }
