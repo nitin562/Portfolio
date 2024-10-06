215f71f98dc5dd31422852726bbb78dfb869cf5e
nitin562 <nitindbas8800@gmail.com>
2024-10-06 14:14:40 +0530

    port update

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express=require("express")
 require("dotenv").config()
 const app=express()
-const port=8000
+const port=process.env.PORT||8000
 const cors=require("cors")
 const connectDb = require("./ConnectDb")
 app.use(express.json())
